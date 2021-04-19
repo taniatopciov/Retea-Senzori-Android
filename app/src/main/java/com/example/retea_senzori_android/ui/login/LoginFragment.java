a1a99c0c13ffebf9198994f66258045a3971fdb4
Ana <anaiv9cs@yahoo.co.uk>
2021-04-19 17:56:20 +0300

    Login improved

--- app/src/main/java/com/example/retea_senzori_android/ui/login/LoginFragment.java
+++ app/src/main/java/com/example/retea_senzori_android/ui/login/LoginFragment.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -29,6 +30,9 @@ public class LoginFragment extends Fragment {
 
         binding = LoginFragmentBinding.inflate(inflater, container, false);
 
+        binding.goToRegister.setOnClickListener(view -> {
+//            Navigation.findNavController(view).navigate(.navigateToRegister());
+        });
         return binding.getRoot();
     }
 
