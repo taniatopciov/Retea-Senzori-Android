a6b6fbfb52968d70e9f76bc9ff7a5359a19718cd
Andrei Timar <atimar999@gmail.com>
2021-05-22 18:06:17 +0300

    Show Paired BT Devices in NodesView

--- app/src/main/java/com/example/retea_senzori_android/nodes/NodesView.java
+++ app/src/main/java/com/example/retea_senzori_android/nodes/NodesView.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.retea_senzori_android.bluetooth.ui.PairedBluetoothDevicesDialogFragment;
 import com.example.retea_senzori_android.databinding.NodesLayoutFragmentBinding;
 
 import java.util.ArrayList;
@@ -16,6 +17,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 public class NodesView extends Fragment {
 
+    private static final int PAIRED_BT_DEVICES_REQUEST_CODE = 310;
+
     private NodesLayoutFragmentBinding binding;
     private ArrayList<NodeModel> nodeArray;
 
@@ -25,17 +28,21 @@ public class NodesView extends Fragment {
         binding = NodesLayoutFragmentBinding.inflate(inflater, container, false);
 
         binding.addNodeFabButton.setOnClickListener(view -> {
+            PairedBluetoothDevicesDialogFragment pairedBluetoothDevicesDialogFragment = PairedBluetoothDevicesDialogFragment.newInstance(device -> {
+                // TODO add Node Service
 
+                System.out.println(device.getName());
+
+//                bluetoothNodeProtocol.connect(device, sdCardErrors -> System.err.println("SDCard Error " + sdCardErrors));
+            });
+            pairedBluetoothDevicesDialogFragment.setTargetFragment(this, PAIRED_BT_DEVICES_REQUEST_CODE);
+            pairedBluetoothDevicesDialogFragment.show(getParentFragmentManager(), "paired_bt_devices_fragment");
         });
 
         nodeArray = new ArrayList<>();
         ArrayList<SensorModel> sensors = new ArrayList<>();
         sensors.add(new SensorModel("Gas"));
         nodeArray.add(new NodeModel("Stolen Node", sensors));
-        //nodeArray.add(new NodeModel("Node1", "Gas", "Rain"));
-        //nodeArray.add(new NodeModel("Funny Node", "Humidity", "Rain"));
-        //nodeArray.add(new NodeModel("My Node", "Rain", "Temperature"));
-        //nodeArray.add(new NodeModel("Stolen Node", "Gas", "Rain"));
         binding.idRVNode.setAdapter(new NodeAdapter(getContext(), nodeArray));
         binding.idRVNode.setLayoutManager(new LinearLayoutManager(getContext()));
 
