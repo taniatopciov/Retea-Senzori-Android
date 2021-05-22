3a93ecce059ee402dff050a493cb2d47677a07eb
Andrei Timar <atimar999@gmail.com>
2021-05-22 11:52:22 +0300

    Added FirebaseRepository

---
+++ app/src/main/java/com/example/retea_senzori_android/persistance/FirebaseRepository.java
@@ -0,0 +1,17 @@
+package com.example.retea_senzori_android.persistance;
+
+import com.example.retea_senzori_android.observables.Subject;
+
+import java.util.List;
+
+public interface FirebaseRepository<T extends FirebaseDocument> {
+    Subject<T> getDocument(String pathToDocument, Class<T> tClass);
+
+    Subject<List<T>> getAllDocuments(String pathToCollection, Class<T> tClass);
+
+    Subject<String> createDocument(String pathToCollection, T document);
+
+    Subject<Boolean> deleteDocument(String pathToCollection, String documentId);
+
+    Subject<Boolean> updateDocument(String pathToDocument, T document);
+}
