d1064a6019613f8c44bc58a69cca06d7ab16afbd
Tania Topciov <taniatopciov@yahoo.com>
2021-05-22 16:03:53 +0300

    Added AuthenticationService in Login and Register

--- app/src/main/java/com/example/retea_senzori_android/authentication/register/RegisterFragment.java
+++ app/src/main/java/com/example/retea_senzori_android/authentication/register/RegisterFragment.java
@@ -12,12 +12,25 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.example.retea_senzori_android.authentication.service.AuthenticationService;
 import com.example.retea_senzori_android.databinding.FragmentRegisterBinding;
+import com.example.retea_senzori_android.di.Injectable;
+import com.example.retea_senzori_android.di.ServiceLocator;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class RegisterFragment extends Fragment {
 
+    @Injectable
+    AuthenticationService authenticationService;
+
     private FragmentRegisterBinding binding;
+
+    public RegisterFragment() {
+        ServiceLocator.getInstance().inject(this);
+    }
 
     public static RegisterFragment newInstance() {
         return new RegisterFragment();
@@ -32,37 +45,51 @@ public class RegisterFragment extends Fragment {
             Navigation.findNavController(view).navigate(RegisterFragmentDirections.navigateToLogin());
         });
 
-//        binding.registerButton.setOnClickListener(view -> {
-//            String email = binding.editTextEmailAddressRegister.getText().toString();
-//            String password = binding.editTextPasswordRegister.getText().toString();
-//            String username = binding.editTextNameRegister.getText().toString();
-//
-//            if (email.isEmpty() || password.isEmpty() || name.isEmpty() || userType.isEmpty()) {
-//                Snackbar.make(view, "Some Fields Are Empty", Snackbar.LENGTH_SHORT)
-//                        .show();
-//                return;
-//            }
-//
-//            if (!validateEmail(email)) {
-//                Snackbar.make(view, "Invalid Email Address", Snackbar.LENGTH_SHORT)
-//                        .show();
-//                return;
-//            }
-//
-//            authenticationService.register(email, password, name, userType, errorMessage -> {
-//                if (errorMessage == null) {
-//                    Intent intent = new Intent();
-//                    intent.putExtra(AUTH_ACTIVITY_REQUEST_EXTRA, "register");
-//                    getActivity().setResult(Activity.RESULT_OK, intent);
-//                    getActivity().finish();
-//                } else {
-//                    Snackbar.make(view, errorMessage, Snackbar.LENGTH_SHORT)
-//                            .show();
-//                }
-//            });
-//        });
+        binding.registerButton.setOnClickListener(view -> {
+            String email = binding.emailRegister.getText().toString();
+            String password = binding.passwordRegister.getText().toString();
+            String confirmPassword = binding.passwordConfirmRegister.getText().toString();
+            String username = binding.usernameRegister.getText().toString();
+
+            if (email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty() || username.isEmpty()) {
+                Snackbar.make(view, "Some Fields Are Empty", Snackbar.LENGTH_SHORT)
+                        .show();
+                return;
+            }
+
+            if(!password.equals(confirmPassword)) {
+                Snackbar.make(view, "Passwords do not match", Snackbar.LENGTH_SHORT)
+                        .show();
+                return;
+            }
+
+            if (!validateEmail(email)) {
+                Snackbar.make(view, "Invalid Email Address", Snackbar.LENGTH_SHORT)
+                        .show();
+                return;
+            }
+
+            authenticationService.register(email, password, username).subscribe(errorMessage -> {
+                if (errorMessage == null) {
+                    Navigation.findNavController(view).navigate(RegisterFragmentDirections.navigateToHomePage());
+                } else {
+                    Snackbar.make(view, errorMessage, Snackbar.LENGTH_SHORT)
+                            .show();
+                }
+            });
+        });
 
         return binding.getRoot();
+    }
+
+    private boolean validateEmail(String email) {
+        Pattern pattern;
+        Matcher matcher;
+        final String EMAIL_PATTERN = "^[_A-Za-z0-9-+]+(.[_A-Za-z0-9-]+)*@" + "[A-Za-z0-9-]+(.[A-Za-z0-9]+)*(.[A-Za-z]{2,})$";
+
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
     }
 
     @Override
