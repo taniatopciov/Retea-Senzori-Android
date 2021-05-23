ff20b4d2af5c21550f1b66d022b922e914273f31
Ana <anaiv9cs@yahoo.co.uk>
2021-05-23 23:13:46 +0300

    Added BehaviourSubject class

--- app/src/main/java/com/example/retea_senzori_android/services/logs/impl/LogsServiceImpl.java
+++ app/src/main/java/com/example/retea_senzori_android/services/logs/impl/LogsServiceImpl.java
@@ -3,12 +3,15 @@ package com.example.retea_senzori_android.services.logs.impl;
 import com.example.retea_senzori_android.bluetooth.protocol.SensorDataLogFile;
 import com.example.retea_senzori_android.models.SensorLogFile;
 import com.example.retea_senzori_android.models.SensorModel;
+import com.example.retea_senzori_android.observables.BehaviourSubject;
 import com.example.retea_senzori_android.observables.Subject;
 import com.example.retea_senzori_android.persistance.FirebaseRepository;
 import com.example.retea_senzori_android.sensor.LogType;
 import com.example.retea_senzori_android.sensor.SensorLogData;
 import com.example.retea_senzori_android.sensor.SensorType;
 import com.example.retea_senzori_android.services.logs.LogsService;
+
+import java.util.ArrayList;
 
 public class LogsServiceImpl implements LogsService {
     private static final String LOGS_DATA_COLLECTION_PATH = "logs";
@@ -24,11 +27,12 @@ public class LogsServiceImpl implements LogsService {
 
     public Subject<SensorLogFile> getLogFromId(String id){
         String pathToLogs = LOGS_DATA_COLLECTION_PATH + "/" + id;
-        Subject<SensorLogFile> subjectLog = new Subject<SensorLogFile>();
+        Subject<SensorLogFile> subjectLog = new BehaviourSubject<SensorLogFile>();
         SensorLogFile slFile = new SensorLogFile();
         SensorDataLogFile sdlFile = new SensorDataLogFile();
         SensorLogData slData = new SensorLogData((byte)2, (byte)3, (short) 3, 4.0f, 12);
         sdlFile.addSensorLogData(slData);
+        slFile.sensorLogs = new ArrayList<>();
         slFile.sensorLogs.add(sdlFile);
         subjectLog.setState(slFile);
 
